2a5f64dd8ebbea081847a1f419db2283c79115e5
Adham Rashidov <adhamrashidov0312@gmail.com>
2026-03-16 20:57:43 +0500

    favicon

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,8 +3,6 @@ import { Contact } from "./sections/contact";
 import { Projects } from "./sections/projects";
 import { Stack } from "./sections/stack";
 
-// import { Hero } from "@/sections/hero";
-
 export const Home = () => {
   return (
     <div className="flex flex-col">
